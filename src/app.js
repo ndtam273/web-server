e951846fc70d53a07dfeb998c6cc032e8525b502
tam nguyen <ndtam.273@gmail.com>
2020-04-28 07:02:05 +0700

    afsdfsf

--- src/app.js
+++ src/app.js
@@ -1,21 +1,9 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
-
-app.get('', ( req, res ) => {
-    res.send('<h1>Weather</h1>')
-} )
-
-app.get('/help', (req, res) => {
-   res.send({
-       name: 'Andrew',
-       age: 27
-   })
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About us</h1>')
-})
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
 
 app.get('/weather', (req, res) => {
     res.send({
